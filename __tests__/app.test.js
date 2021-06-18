49beb346a769907cfd9a3b6cd8f8ec12aaec060c
Sofia Tejada <sofianais@icloud.com>
2021-06-18 11:46:12 -0700

    got the put test to work!

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -6,91 +6,6 @@ const fakeRequest = require('supertest');
 const app = require('../lib/app');
 const client = require('../lib/client');
 
-const legends = [
-  {
-    name: 'Galarian Articuno',
-    special_type: 'Sub-Legendary',
-    special_group: 'Galarian Legendary Birds',
-    type_1: 'Psychic',
-    type_2: 'Flying',
-    other_forms: true,
-  },
-  {
-    name: 'Galarian Zapdos',
-    special_type: 'Sub-Legendary',
-    special_group: 'Glarian Legendary Birds',
-    type_1: 'Fighting',
-    type_2: 'Flying',
-    other_forms: true,
-  },
-  {
-    name: 'Galarian Moltres',
-    special_type: 'Sub-Legendary',
-    special_group: 'Galarian Legendary Birds',
-    type_1: 'Dark',
-    type_2: 'Flying',
-    other_forms: true,
-  },
-  {
-    name: 'Cosmog',
-    special_type: 'Legendary',
-    special_group: 'Other',
-    type_1: 'Psychic',
-    type_2: 'None',
-    other_forms: false,
-  },
-  {
-    name: 'Tapu Koko',
-    special_type: 'Sub-Legendary',
-    special_group: 'Guardian Dieties',
-    type_1: 'Electric',
-    type_2: 'Fairy',
-    other_forms: false,
-  },
-  {
-    name: 'Tapu Lele',
-    special_type: 'Sub-Legendary',
-    special_group: 'Guardian Dieties',
-    type_1: 'Psychic',
-    type_2: 'Fairy',
-    other_forms: false,
-  },
-  {
-    name: 'Tapu Bulu',
-    natid: 787,
-    special_type: 'Sub-Legendary',
-    special_group: 'Guardian Dieties',
-    type_1: 'Grass',
-    type_2: 'Fairy',
-    other_forms: false,
-  },
-  {
-    name: 'Tapu Fini',
-    special_type: 'Sub-Legendary',
-    special_group: 'Guardian Dieties',
-    type_1: 'Water',
-    type_2: 'Fairy',
-    other_forms: false,
-  },
-  {
-    name: 'Celebi',
-    special_type: 'Mythical',
-    special_group: 'N/A',
-    type_1: 'Psychic',
-    type_2: 'Grass',
-    other_forms: false,
-  },
-  {
-    name: 'Mega Diancie',
-    special_type: 'Mythical',
-    special_group: 'N/A',
-    type_1: 'Rock',
-    type_2: 'Fairy',
-    other_forms: true,
-  },
-];
-
-
 describe('app routes', () => {
   describe('routes', () => {
     let token;
@@ -116,130 +31,182 @@ describe('app routes', () => {
       return client.end(done);
     });
 
-    // test('returns animals', async() => {
+    // test('POST', async() => {
+    //   const data = await fakeRequest(app)
+    //     .post('/legends')
+    //     .send({
+    //       name: 'Pikachu',
+    //       special_type: 'none',
+    //       special_group: 'none',
+    //       type_1: 'electric',
+    //       type_2: 'none',
+    //       other_forms: false
+    //     })
+    //     .expect('Content-Type', /json/)
+    //     .expect(200);
 
+    //   const dataLegends = await fakeRequest(app)
+    //     .get('/legends')
+    //     .expect('Content-Type', /json/)
+    //     .expect(200);
+
+    //   const newLegend = {
+    //     'id': 12,
+    //     'name': 'Pikachu',
+    //     'special_type': 'none',
+    //     'special_group': 'none',
+    //     'type_1': 'electric',
+    //     'type_2': 'none',
+    //     'other_forms': false,
+    //     'owner_id': 1
+    //   };
+
+    //   expect(data.body).toEqual(newLegend);
+    //   expect(dataLegends.body).toContainEqual(newLegend);
+    // });
+
+
+    // test('GET', async() => {
     //   const expectation = [
     //     {
     //       'id': 1,
-    //       'name': 'bessie',
-    //       'coolfactor': 3,
+    //       'name': 'Galarian Articuno',
+    //       'special_type': 'Sub-Legendary',
+    //       'special_group': 'Galarian Legendary Birds',
+    //       'type_1': 'Psychic',
+    //       'type_2': 'Flying',
+    //       'other_forms': true,
     //       'owner_id': 1
     //     },
     //     {
     //       'id': 2,
-    //       'name': 'jumpy',
-    //       'coolfactor': 4,
+    //       'name': 'Galarian Zapdos',
+    //       'special_type': 'Sub-Legendary',
+    //       'special_group': 'Glarian Legendary Birds',
+    //       'type_1': 'Fighting',
+    //       'type_2': 'Flying',
+    //       'other_forms': true,
     //       'owner_id': 1
     //     },
     //     {
     //       'id': 3,
-    //       'name': 'spot',
-    //       'coolfactor': 10,
+    //       'name': 'Galarian Moltres',
+    //       'special_type': 'Sub-Legendary',
+    //       'special_group': 'Galarian Legendary Birds',
+    //       'type_1': 'Dark',
+    //       'type_2': 'Flying',
+    //       'other_forms': true,
+    //       'owner_id': 1
+    //     },
+    //     {
+    //       'id': 4,
+    //       'name': 'Cosmog',
+    //       'special_type': 'Legendary',
+    //       'special_group': 'Other',
+    //       'type_1': 'Psychic',
+    //       'type_2': 'None',
+    //       'other_forms': false,
+    //       'owner_id': 1
+    //     },
+    //     {
+    //       'id': 5,
+    //       'name': 'Tapu Koko',
+    //       'special_type': 'Sub-Legendary',
+    //       'special_group': 'Guardian Dieties',
+    //       'type_1': 'Electric',
+    //       'type_2': 'Fairy',
+    //       'other_forms': false,
+    //       'owner_id': 1
+    //     },
+    //     {
+    //       'id': 6,
+    //       'name': 'Tapu Lele',
+    //       'special_type': 'Sub-Legendary',
+    //       'special_group': 'Guardian Dieties',
+    //       'type_1': 'Psychic',
+    //       'type_2': 'Fairy',
+    //       'other_forms': false,
+    //       'owner_id': 1
+    //     },
+    //     {
+    //       'id': 7,
+    //       'name': 'Tapu Bulu',
+    //       'special_type': 'Sub-Legendary',
+    //       'special_group': 'Guardian Dieties',
+    //       'type_1': 'Grass',
+    //       'type_2': 'Fairy',
+    //       'other_forms': false,
+    //       'owner_id': 1
+    //     },
+    //     {
+    //       'id': 8,
+    //       'name': 'Tapu Fini',
+    //       'special_type': 'Sub-Legendary',
+    //       'special_group': 'Guardian Dieties',
+    //       'type_1': 'Water',
+    //       'type_2': 'Fairy',
+    //       'other_forms': false,
+    //       'owner_id': 1
+    //     },
+    //     {
+    //       'id': 9,
+    //       'name': 'Celebi',
+    //       'special_type': 'Mythical',
+    //       'special_group': 'N/A',
+    //       'type_1': 'Psychic',
+    //       'type_2': 'Grass',
+    //       'other_forms': false,
+    //       'owner_id': 1
+    //     },
+    //     {
+    //       'id': 10,
+    //       'name': 'Mega Diancie',
+    //       'special_type': 'Mythical',
+    //       'special_group': 'N/A',
+    //       'type_1': 'Rock',
+    //       'type_2': 'Fairy',
+    //       'other_forms': true,
+    //       'owner_id': 1
+    //     },
+    //     {
+    //       'id': 11,
+    //       'name': 'Pikachu',
+    //       'special_type': 'none',
+    //       'special_group': 'none',
+    //       'type_1': 'electric',
+    //       'type_2': 'none',
+    //       'other_forms': false,
+    //       'owner_id': 1
+    //     },
+    //     {
+    //       'id': 12,
+    //       'name': 'Pikachu',
+    //       'special_type': 'none',
+    //       'special_group': 'none',
+    //       'type_1': 'electric',
+    //       'type_2': 'none',
+    //       'other_forms': false,
     //       'owner_id': 1
     //     }
     //   ];
 
     //   const data = await fakeRequest(app)
-    //     .get('/animals')
+    //     .get('/legends')
     //     .expect('Content-Type', /json/)
     //     .expect(200);
 
     //   expect(data.body).toEqual(expectation);
     // });
 
-    const legends = [
-      {
-        name: 'Galarian Articuno',
-        special_type: 'Sub-Legendary',
-        special_group: 'Galarian Legendary Birds',
-        type_1: 'Psychic',
-        type_2: 'Flying',
-        other_forms: true,
-      },
-      {
-        name: 'Galarian Zapdos',
-        special_type: 'Sub-Legendary',
-        special_group: 'Glarian Legendary Birds',
-        type_1: 'Fighting',
-        type_2: 'Flying',
-        other_forms: true,
-      },
-      {
-        name: 'Galarian Moltres',
-        special_type: 'Sub-Legendary',
-        special_group: 'Galarian Legendary Birds',
-        type_1: 'Dark',
-        type_2: 'Flying',
-        other_forms: true,
-      },
-      {
-        name: 'Cosmog',
-        special_type: 'Legendary',
-        special_group: 'Other',
-        type_1: 'Psychic',
-        type_2: 'None',
-        other_forms: false,
-      },
-      {
-        name: 'Tapu Koko',
-        special_type: 'Sub-Legendary',
-        special_group: 'Guardian Dieties',
-        type_1: 'Electric',
-        type_2: 'Fairy',
-        other_forms: false,
-      },
-      {
-        name: 'Tapu Lele',
-        special_type: 'Sub-Legendary',
-        special_group: 'Guardian Dieties',
-        type_1: 'Psychic',
-        type_2: 'Fairy',
-        other_forms: false,
-      },
-      {
-        name: 'Tapu Bulu',
-        natid: 787,
-        special_type: 'Sub-Legendary',
-        special_group: 'Guardian Dieties',
-        type_1: 'Grass',
-        type_2: 'Fairy',
-        other_forms: false,
-      },
-      {
-        name: 'Tapu Fini',
-        special_type: 'Sub-Legendary',
-        special_group: 'Guardian Dieties',
-        type_1: 'Water',
-        type_2: 'Fairy',
-        other_forms: false,
-      },
-      {
-        name: 'Celebi',
-        special_type: 'Mythical',
-        special_group: 'N/A',
-        type_1: 'Psychic',
-        type_2: 'Grass',
-        other_forms: false,
-      },
-      {
-        name: 'Mega Diancie',
-        special_type: 'Mythical',
-        special_group: 'N/A',
-        type_1: 'Rock',
-        type_2: 'Fairy',
-        other_forms: true,
-      },
-    ];
-
-    test('stuff', async() => {
+    test('PUT', async() => {
       const data = await fakeRequest(app)
-        .post('/legends')
+        .put('/legends/10')
         .send({
-          name: 'Pikachu',
-          special_type: 'none',
-          special_group: 'none',
-          type_1: 'electric',
-          type_2: 'none',
+          name: 'Cosmoem',
+          special_type: 'Legendary Pokemon',
+          special_group: 'Other',
+          type_1: 'Psychic',
+          type_2: 'None',
           other_forms: false
         })
         .expect('Content-Type', /json/)
@@ -251,12 +218,12 @@ describe('app routes', () => {
         .expect(200);
 
       const newLegend = {
-        'id': 11,
-        'name': 'Pikachu',
-        'special_type': 'none',
-        'special_group': 'none',
-        'type_1': 'electric',
-        'type_2': 'none',
+        'id': 10,
+        'name': 'Cosmoem',
+        'special_type': 'Legendary Pokemon',
+        'special_group': 'Other',
+        'type_1': 'Psychic',
+        'type_2': 'None',
         'other_forms': false,
         'owner_id': 1
       };
